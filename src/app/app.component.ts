09d176597da8b0996611a125a01792562b672f48
Yukthiprabhu <yukthi@example.com>
2023-08-10 22:34:42 +0530

    initialize

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'project';
+  constructor(){}
+  FirstName:string='Anurag';
+  LastName:string='Shatul';
+  Age:number=45
+}
+
